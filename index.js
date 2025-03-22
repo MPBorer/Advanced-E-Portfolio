dfc6dd91c2ab6cbc86aa6c90acc64b2d562f9b7e
Michael <mpborer@gmail.com>
2025-03-22 14:48:11 -0600

    integrated/email.js-done

--- index.js
+++ index.js
@@ -2,20 +2,22 @@
 
 function contact(event) {
     event.preventDefault();
-    // emailjs
-    // .sendForm(
-    //     `service_20usx8r`,
-    //     `template_ioadkck`,
-    //     event.target,
-    //     `M5cUpznJLQfGLMsMh`
-    // ).then(() => {
-    //     console.log(`this worked1`)
-    // })
     const loading = document.querySelector(`.modal__overlay--loading`)
     const success = document.querySelector(`.modal__overlay--success`) 
-    loading.classlist += " modal__overlay--visible"
-    setTimeout(() => {
-        console.log(`it worked 1`)
-    }, 500)
-
+    loading.classList += " modal__overlay--visible";
+    emailjs
+    .sendForm(
+        `service_20usx8r`,
+        `template_ioadkck`,
+        event.target,
+        `M5cUpznJLQfGLMsMh`
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The Email service is temporarily unavailable.  Please contact me directly @ MPBorer@gmail.com"
+        )
+    })
 }
