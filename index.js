55831219265c0717c88b756840af1b26d3877b33
Michael <mpborer@gmail.com>
2025-03-24 10:43:28 -0600

    dark-mode

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+let isModalOpen = false;
+let contrastToggle = false 
 //
-//
-//
+
+function toggleContrast(){
+    contrastToggle = !contrastToggle;
+    if (contrastToggle){
+        document.body.classList += " dark-theme";
+    }
+    else{
+        document.body.classList.remove("dark-theme")
+    }
+}
 
 function contact(event) {
     event.preventDefault();
@@ -24,11 +34,11 @@ function contact(event) {
     })
 }
 
-let isModalOpen = false;
+
 function toggleModal () {
     if (isModalOpen) {
-        isModalOpen = false
-        return document.body.classList.remove("modal--open")
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
     }
     isModalOpen = true;
     document.body.classList += "modal--open";
